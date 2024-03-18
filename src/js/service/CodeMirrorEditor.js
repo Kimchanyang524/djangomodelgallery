fde9d96be1875f4e25cddbe11b926dba4610d912
HR. Lee <matt14507@gmail.com>
2024-03-18 18:28:37 +0900

    feat: Codemirror Class와 mermaid codeblock 연동

---
+++ src/js/service/CodeMirrorEditor.js
@@ -0,0 +1,82 @@
+export default class CodeMirrorEditor {
+  constructor(selector, initialValue = "") {
+    this.selector = selector;
+    this.initialValue = initialValue;
+    this.editor = null;
+  }
+
+  // CodeMirror 스크립트와 CSS 동적 추가 함수
+  async loadCodeMirror() {
+    return new Promise((resolve, reject) => {
+      // CodeMirror CSS 동적 추가
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href =
+        "https://cdn.jsdelivr.net/npm/codemirror@5.63.3/lib/codemirror.min.css";
+      document.head.appendChild(link);
+
+      // 추가적인 스타일시트 로드
+      const themeLink = document.createElement("link");
+      themeLink.rel = "stylesheet";
+      themeLink.href =
+        "https://cdn.jsdelivr.net/npm/codemirror/theme/dracula.min.css";
+      document.head.appendChild(themeLink);
+
+      // CodeMirror 스크립트 동적 추가
+      const script = document.createElement("script");
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.4/codemirror.min.js";
+      script.onload = () => {
+        // 추가적인 스크립트 로드
+        const markdownScript = document.createElement("script");
+        markdownScript.src =
+          "https://cdn.jsdelivr.net/npm/codemirror@5.63.3/mode/markdown/markdown.min.js";
+        markdownScript.onload = () => {
+          console.log("CodeMirror loaded");
+          resolve();
+        };
+        markdownScript.onerror = (e) => {
+          console.error("Error loading CodeMirror markdown mode:", e);
+          reject(e);
+        };
+        document.body.appendChild(markdownScript);
+      };
+      script.onerror = (e) => {
+        console.error("Error loading CodeMirror:", e);
+        reject(e);
+      };
+      document.body.appendChild(script);
+    });
+  }
+  async initialize() {
+    try {
+      await this.loadCodeMirror();
+      const CodemirrorTextarea = document.querySelector(this.selector);
+      if (CodemirrorTextarea) {
+        this.editor = CodeMirror.fromTextArea(CodemirrorTextarea, {
+          lineNumbers: true,
+          mode: "text/x-markdown",
+          theme: "dracula",
+        });
+        this.setValue(this.initialValue);
+      } else {
+        console.error("Textarea for CodeMirror not found:", this.selector);
+      }
+    } catch (e) {
+      console.error("Error initializing CodeMirror:", e);
+    }
+  }
+
+  setValue(value) {
+    if (this.editor) {
+      this.editor.setValue(value);
+    }
+  }
+
+  getValue() {
+    if (this.editor) {
+      return this.editor.getValue();
+    }
+    return null;
+  }
+}
--- src/js/views/postDetailView.js
+++ src/js/views/postDetailView.js
@@ -1,4 +1,5 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import CodeMirrorEditor from "../service/CodeMirrorEditor.js";
 
 export async function createPostDetailView(fileName) {
   try {
@@ -9,19 +10,44 @@ export async function createPostDetailView(fileName) {
       throw new Error("Failed to fetch post detail");
     }
     const markdownContent = await response.text();
+    const mermaidCodeBlocks = [];
 
-    // marked 라이브러리 동적 로드
-    // await loadMarkedLibrary();
+    marked.use({
+      renderer: {
+        code(code, infostring) {
+          if (infostring === "mermaid") {
+            mermaidCodeBlocks.push(code);
+            return '<div class="mermaid-placeholder"></div>';
+          }
+          return `<pre><code class="${infostring}">${code}</code></pre>`;
+        },
+      },
+    });
 
     const htmlContent = marked.parse(markdownContent);
+    const postDetailSection = createPostDetailElement(
+      htmlContent,
+      mermaidCodeBlocks.join("\n") // 여러 개의 mermaid 코드 블록을 하나의 문자열로 결합
+    );
 
-    const postDetailSection = document.createElement("section");
-    postDetailSection.innerHTML = `
-            <div class="prose">${htmlContent}</div>
-        `;
+    postDetailSection.mermaidCodeBlocks = mermaidCodeBlocks;
     return postDetailSection;
   } catch (error) {
     console.error("Error:", error);
     return null;
   }
+}
+
+function createPostDetailElement(htmlContent, mermaidCodeBlocks) {
+  const postDetailSection = document.createElement("section");
+  postDetailSection.innerHTML = `
+        <article class="prose">${htmlContent}</article>
+        <div class="mermaid-code-editor-container"></div> 
+        <Textarea class="language-mermaid"></Textarea>
+    `;
+
+  const editor = new CodeMirrorEditor(".language-mermaid", mermaidCodeBlocks);
+  editor.initialize();
+
+  return postDetailSection;
 }
