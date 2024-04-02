6618d193dd7f06d0eb35b1f6dad8fc8a2f1dc86c
KCY <cks402@naver.com>
2024-04-02 21:56:27 +0900

    update_json 인코딩 변경

--- src/python/update_json.py
+++ src/python/update_json.py
@@ -1,23 +1,19 @@
-import sys
-import json
 import os
+import glob
 
 
 def read_file(file):
     if os.path.exists(file):
-        with open(file, "r") as f:
+        with open(file, "r", encoding="utf-8") as f:
             return f.read()
     else:
         return f"Error: {file} does not exist."
 
 
 def main():
-    files = "src/posts/*.md"
-
-    result = {}
-    for file in files:
-        result[file] = read_file(file).split()
-    print(result)
+    for file_path in glob.glob("src/posts/*.md"):
+        content = read_file(file_path)
+        print(content)
 
 
 if __name__ == "__main__":
